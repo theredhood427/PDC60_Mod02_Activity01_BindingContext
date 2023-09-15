b24f263dbbbc7f3f39bf32a1f3b25814f9adf7a6
MISS <rrbruss001@gmail.com>
2023-09-15 11:31:52 +0800

    First Commit

---
+++ PDC06_Module02/PDC06_Module02/Model/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC06_Module02.Model
+{
+    class TaskModel
+    {
+        public string SubjectCode { get; set; }
+
+        public string SubjectTitle { get; set; }
+
+        public int Unit { get; set; }
+    }
+}
---
+++ PDC06_Module02/PDC06_Module02/ViewModel/TaskViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PDC06_Module02.Model;
+
+namespace PDC06_Module02.ViewModel
+{
+    class TaskViewModel
+    {
+        public TaskModel TaskModelSet { get; set; }
+
+        public TaskViewModel()
+        {
+            TaskModelSet = new TaskModel
+            {
+                SubjectCode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+            };
+        }
+    }
+}
